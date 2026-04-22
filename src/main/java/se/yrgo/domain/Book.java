1c6cd5537a35411b4f90fa52f312991825fdc7de
frogstat <frogstatftw@gmail.com>
2026-04-22 10:48:36 +0200

    create basic structure

---
+++ src/main/java/se/yrgo/domain/Book.java
@@ -0,0 +1,4 @@
+package se.yrgo.domain;
+
+public class Book {
+}
---
+++ src/main/java/se/yrgo/domain/BookCopy.java
@@ -0,0 +1,10 @@
+package se.yrgo.domain;
+
+public class BookCopy {
+
+    private Book book;
+
+    public BookCopy(Book book){
+        this.book = book;
+    }
+}
