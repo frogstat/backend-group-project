1c2af8f800693109f0543a3ea37780b3bf3e3bb1
Josephine Jelkeby Ginguene <josephine.ginguene@gmail.com>
2026-04-22 13:30:49 +0200

    Creating Author and Book class
    # Conflicts:
    #	src/main/java/se/yrgo/domain/Book.java

--- src/main/java/se/yrgo/domain/Author.java
+++ src/main/java/se/yrgo/domain/Author.java
@@ -1,4 +1,40 @@
 package se.yrgo.domain;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    @ManyToMany(mappedBy = "authors")
+    private List<Book> books;
+
+    public Author(String name) {
+        this.name = name;
+        books = new ArrayList<>();
+    }
+
+    public Author() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void addWrittenBook(Book book) {
+        if (!books.contains(book)) {
+            books.add(book);
+            book.addAuthor(this);
+        }
+    }
 }
